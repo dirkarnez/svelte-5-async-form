abe0d4892f2763d630ea85628a4a6b40b66df040
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-23 23:12:33 +0800

    - update index.svelte

--- index.svelte
+++ index.svelte
@@ -13,11 +13,24 @@
 			})
 		}
 	});
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		const response = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+	    method: "POST",
+	    body: formData,
+	  });
+		const data = await response.json();
+		alert(JSON.stringify(data));
+	}
 </script>
 
-<label>ID:
-<input type="number"  bind:value={obj.id} min="0" max="10" />
-</label>
+<form method="post" enctype="multipart/form-data" onsubmit={handleSubmit}>
+	<label>ID:
+	<input type="number" bind:value={obj.id} min="0" max="10" />
+	</label>
+	<input type="submit" value="submit">
+</form>
 <br>
 {JSON.stringify(obj)}
-
