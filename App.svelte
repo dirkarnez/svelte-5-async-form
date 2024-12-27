4e9aea290a444549896c1ccc8f5adc32a65b234f
Dirk Arnez <smalldirkalex@gmail.com>
2024-12-27 23:21:33 +0800

    - update App.svelte

--- App.svelte
+++ App.svelte
@@ -1,3 +1,9 @@
+<svelte:head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
+</svelte:head>
+
 <script>
 	let obj = $state({id: 0});
 
@@ -6,25 +12,15 @@
 			new Promise(res => {
 				setTimeout(res, 2000)
 			})
-			.then(() => fetch(`https://jsonplaceholder.typicode.com/todos/${obj.id}`))
+			.then(() => fetch(`http://localhost/hi`))
       .then(response => response.json())
       .then(json => {
+				debugger;
 				obj = {...json, id: obj.id};
 			})
 		}
 	});
 
-	/* localhost api for
-
-	PHP
-	function enableCORS(Response $response) {
-	    return $response
-		->withHeader('Content-Type', 'application/json')
-		->withHeader('Access-Control-Allow-Origin', '*')
-		->withHeader('Access-Control-Allow-Headers', '*')
-		->withHeader('Access-Control-Allow-Methods', '*');
-	}
-	*/
 	const handleSubmit = async e => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
@@ -35,13 +31,33 @@
 		const data = await response.json();
 		alert(JSON.stringify(data));
 	}
-</script>
 
-<form method="post" enctype="multipart/form-data" onsubmit={handleSubmit}>
-	<label>ID:
-	<input type="number" bind:value={obj.id} min="0" max="10" />
-	</label>
-	<input type="submit" value="submit">
-</form>
+	const json = {
+		
+	}
+</script>
+<div class="container" style="padding: 1rem">
+	<div class="columns">
+	  <div class="column">
+			<form method="post" enctype="multipart/form-data" onsubmit={handleSubmit}>
+				<!-- <label>ID:
+				<input type="number" bind:value={obj.id} min="0" max="10" />
+				</label> -->
+				{#each [ "a", "b", "c" ] as name}
+					<div class="field">
+						<label class="label" for="name">Name</label>
+						<div class="control">
+							<input class="input" id="name" type="text" placeholder="">
+						</div>
+					</div>
+				{/each}
+				<input type="submit" value="submit">
+			</form>
+		</div>
+	</div>
+</div>
+
+
+
 <br>
 {JSON.stringify(obj)}
